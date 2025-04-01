5dc5fda41bb5b5623965ebc67ec5b836a3b8694d
sigurolo <olov.sigurdson@tietoevry.com>
2025-04-01 15:22:09 +0200

    Testar igen

--- StreamlitDashboards.py
+++ StreamlitDashboards.py
@@ -1,9 +1,23 @@
+#import pandas
 import streamlit as st
+
+# data = {
+#     'Series_1': [1, 2, 3, 4, 5],
+#     'Series_2': [10, 30, 40, 100, 250]
+# }
+#
+# print(data)
+
+#df = pandas.DataFrame(data)
+
 
 st.title('Olovs first app')
 st.subheader('Testar Streamlit!!')
 st.write('''Första webappen
 Testa
+
+Jajamän, testar!
 ''')
+
 
 
