18d39115ec76954b689411908bec8a26a65c1063
sigurolo <olov.sigurdson@tietoevry.com>
2025-04-10 16:23:53 +0200

    Currency converter new design

--- pyqt6_impr_gui_currencyconverter.py
+++ pyqt6_impr_gui_currencyconverter.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout
 from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit, QComboBox
+from PyQt6.QtCore import Qt
 from BScurrencyconverter import get_currency
 
 
@@ -19,6 +20,12 @@ window = QWidget()
 window.setWindowTitle("Konvertera valuta")
 #Layout
 layout = QVBoxLayout()
+layout1 = QHBoxLayout()
+layout2 = QVBoxLayout()
+layout3 = QVBoxLayout()
+layout.addLayout(layout1)
+layout1.addLayout(layout2)
+layout1.addLayout(layout3)
 
 #Två textboxar
 text = QLineEdit()
@@ -35,19 +42,17 @@ combo.addItems(["SEK", "EUR", "USD"])
 
 #Kombobox 2
 combo2 = QComboBox()
-combo2.addItem("SEK")
-combo2.addItem("EUR")
-combo2.addItem("USD")
+combo2.addItems(["EUR", "SEK", "USD"])
 
 #widget, signal, slot - funktion
 #Knapp och combo
 button.clicked.connect(convert_currency)
 
 #Lägger ut layout
-layout.addWidget(text)
-layout.addWidget(combo)
-layout.addWidget(combo2)
-layout.addWidget(button)
+layout3.addWidget(text)
+layout2.addWidget(combo)
+layout2.addWidget(combo2)
+layout3.addWidget(button, alignment=Qt.AlignmentFlag.AlignBottom)
 layout.addWidget(label)
 
 
